fc0d0611982062d4f34cd82ca78d58e0baeaefec
smwasifzamanomee <wasifbdjsr@gmail.com>
2023-03-28 19:35:31 +0600

    fixed some issue

--- books/views.py
+++ books/views.py
@@ -1,9 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+todo = [
+    {"todo": "Learn Python"},
+    {"todo": "Learn Django"},
+    {"todo": "Learn React"},
+]
+
 def index(request):
     data = "Hello, world. You're at the books index."
-    return HttpResponse(data)
+    return render(request, 'index.html', {'todo': todo})
 
 def Homepage(request):
     data1 = "<h1>Homepage</h1>"
@@ -18,13 +24,9 @@ def Homepage(request):
             
     return HttpResponse(data1)
 
-todo = [
-    {"todo": "Learn Python"},
-    {"todo": "Learn Django"},
-    {"todo": "Learn React"},
-]
+
 
 def Firstpage(request):
     user = "Wasif Zaman Omee"
-    return render(request, 'index.html', {'user': todo})
+    return render(request, 'index.html', {'user': user})
     
