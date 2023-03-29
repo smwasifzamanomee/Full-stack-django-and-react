ee9b2fc795ca17311c4272bbe7bc35a39e2052b9
smwasifzamanomee <wasifbdjsr@gmail.com>
2023-03-29 20:02:53 +0600

    nav and card added

--- books/urls.py
+++ books/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('posts/', include('socialblogposts.urls')),
+    path('', include('socialblogposts.urls')),
     path('users/', include('users.urls')),
 ]
