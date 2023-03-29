27d07a18b5327029aecb1216feefa6102d787d71
smwasifzamanomee <wasifbdjsr@gmail.com>
2023-03-29 14:39:04 +0600

    project init

--- books/urls.py
+++ books/urls.py
@@ -1,11 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from todolist1 import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('todolist1/', views.index),
-    path('todolist1/delete/<int:id>/', views.delete),
-    path('todolist1/update/<int:id>/', views.update),
-    path('todolist1/updateData/', views.updateData)
 ]
