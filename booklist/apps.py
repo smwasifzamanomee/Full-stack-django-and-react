3856bb4ba6536fcace4ada2ef6b9046fd3b3a671
smwasifzamanomee <wasifbdjsr@gmail.com>
2023-03-31 09:29:35 +0600

    init

--- booklist/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class BooklistConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'booklist'
--- books/views.py
+++ books/views.py
@@ -1,30 +1,5 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-def index(request):
-    data = "Hello, world. You're at the books index."
-    return HttpResponse(data)
-
-def Homepage(request):
-    data1 = "<h1>Homepage</h1>"
-    if request.method == "POST":
-        username = request.POST['username']
-        password = request.POST['password']
-        data = {
-            "username": username,
-            "password": password
-        }
-        return render(request, 'index.html', {'data': data})
-            
-    return HttpResponse(data1)
-
-todo = [
-    {"todo": "Learn Python"},
-    {"todo": "Learn Django"},
-    {"todo": "Learn React"},
-]
-
-def Firstpage(request):
-    user = "Wasif Zaman Omee"
-    return render(request, 'index.html', {'user': todo})
+# Create your views here.
     
